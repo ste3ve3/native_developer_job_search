df4a693ffb8763b6d30d2ef123389578a4830e29
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2023-07-10 19:32:23 +0200

    first commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                Steve Ndicunguye
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
